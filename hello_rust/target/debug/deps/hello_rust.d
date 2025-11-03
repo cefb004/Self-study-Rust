d58120fced0fee6ecbae80ad94a3a2eb6331275b
Carlos Brandao <cefb004@gmail.com>
2025-11-03 19:59:51 -0300

    Hello World!

---
+++ hello_rust/target/debug/deps/hello_rust.d
@@ -0,0 +1,5 @@
+C:\Users\Carlos\self-study-rust\hello_rust\target\debug\deps\hello_rust.d: src\main.rs
+
+C:\Users\Carlos\self-study-rust\hello_rust\target\debug\deps\hello_rust.exe: src\main.rs
+
+src\main.rs:
---
+++ hello_rust/target/debug/hello_rust.d
@@ -0,0 +1,1 @@
+C:\Users\Carlos\self-study-rust\hello_rust\target\debug\hello_rust.exe: C:\Users\Carlos\self-study-rust\hello_rust\src\main.rs
